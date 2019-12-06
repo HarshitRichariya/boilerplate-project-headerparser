ae5258b2ace84adfe10f33c080477a737ff29d83
Harshit Richariya <35799384+HarshitRichariya@users.noreply.github.com>
2019-12-06 17:41:40 +0530

    Project completed

--- server.js
+++ server.js
@@ -24,7 +24,17 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/whoami", (req, res) => {
+  const ipAddress = req.headers['x-forwarded-for'].split(',')[0];
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  
+  res.json({
+    "ipaddress": ipAddress,
+    "language": language,
+    "software": software
+  })
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
